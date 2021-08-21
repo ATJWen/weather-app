c122f550482ff9690fc1f3418cf5a15fbb72e153
ATJWen <atjwen@outlook.my>
2021-08-21 23:19:14 +0100

    created flask routes and simple app

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 #IMPORTS
-from tkinter import *
+from pyowm.weatherapi25 import location
 from weather_with_wrapper import Weather
+from flask import Flask, render_template, request, redirect, url_for
 
 #-------------------------------------------------
-#GUI
-# root = Tk()
-# root.title('Weather')
-# root.geometry("600x400")
+app = Flask(__name__)
 
-# root.mainloop()
+@app.route('/weather/<city>')
+def print_current_weather(city):
+    w = Weather(city)
+    #pass all information here
+    return render_template('weather.html', 
+        location = city, 
+        cloud_status = str(w.get_current_cloud_status().detailed_status),
+        current_temp = str(w.get_current_temp_info()['temp'])
+    )
 
-w = Weather('Tralee,IE')
-w.get_current_cloud_status()
+@app.route('/select_location')
+def select_location():
+    return render_template('index.html')
+
+@app.route('/index',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      selected_location = request.form['selected_location']
+      return redirect(url_for('print_current_weather',city = selected_location))
+   else:
+      return None
+
+if __name__ == '__main__':
+    app.run()
--- weather_with_wrapper.py
+++ weather_with_wrapper.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from pyowm.owm import OWM
 from pyowm.weatherapi25 import weather
+from pyowm.utils import timestamps, formatting
 import json
 
 #----------------------------------------------------------------------
@@ -20,16 +21,17 @@ class Weather:
 #----------------------------------------------------------------------
     #FUNCTIONS
     def jprint(obj):
-            text = json.dumps(obj, sort_keys=True, indent=4)
-            print(text)
+        text = json.dumps(obj, sort_keys=True, indent=4)
+        print(text)
 
 #----------------------------------------------------------------------
     #GET CURRENT WEATHER
+    #GET CLOUD STATUS
     def get_current_cloud_status(self):
-        print(self.weather.status) #CLOUDS
-        print(self.weather.detailed_status) #DETAILED DESCRIPTION OF CLOUDS
+        # print(self.weather.status) #CLOUDS
+        # print(self.weather.detailed_status) #DETAILED DESCRIPTION OF CLOUDS
+        return self.weather
 
-#----------------------------------------------------------------------
     #GET MIN-MAX TEMP IN CELSIUS OF THE DAY
     def get_current_temp_info(self):
         temp_dict_celsius = self.weather.temperature('celsius')
@@ -38,26 +40,55 @@ class Weather:
         temp_dict_celsius['feels_like'] #CURRENT WEATHER FEEL
         temp_dict_celsius['temp'] #CURRENT ACTUAL WEATHER
         # print(temp_dict_celsius)
+        return temp_dict_celsius
 
-#----------------------------------------------------------------------
     #GET CURRENT WIND INFO
     def get_current_wind_info(self):
         wind_dict_in_mps = self.weather.wind()
         wind_dict_in_mps['speed'] #CURRENT WIND SPEED
         wind_dict_in_mps['deg'] #DIRECTION OF WIND
         # print(wind_dict_in_mps)
+        return wind_dict_in_mps
 
-#----------------------------------------------------------------------
     #GET CURRENT RAIN AMOUNT (MM)
     def get_current_rain_info(self):
         rain = self.weather.rain
-        # if bool(rain):
-        #     print(rain)
-        # else:
-        #     print("NO RAIN DATA FOR THE PAST HOUR")
+        if bool(rain):
+            # print(rain)
+            return rain
+        else:
+            # print("NO RAIN DATA FOR THE PAST HOUR")
+            return "NO RAIN DATA FOR THE PAST HOUR"
 
 #---------------------------------------------------------------------
     #FORECASTING
-    def get_forecast(self):
+    def get_forecast_datetime(self):
+        forecast_date = []
         for daily_weather in self.forecast:
-            print(daily_weather.temp['temp_max'])
+            # print(formatting.to_ISO8601(daily_weather.ref_time))
+            forecast_date.append(formatting.to_ISO8601(daily_weather.ref_time))
+        return forecast_date
+
+    def get_forecast_status(self):
+        for daily_weather in self.forecast:
+            print("STATUS: " + daily_weather.status + ", DETAILED STATUS: " + daily_weather.detailed_status)
+
+    def get_forecast_temp(self):
+        for daily_weather in self.forecast:
+            forecast_temp_celsius = daily_weather.temperature('celsius')
+            print(forecast_temp_celsius['temp'])
+
+    def get_forecast_wind(self):
+        for daily_weather in self.forecast:
+            print(daily_weather.wnd['speed'])
+
+    def get_forecast_rain(self):
+        for daily_weather in self.forecast:
+            if bool(daily_weather.rain):
+                print(daily_weather.rain['3h'])
+            else:
+                print("NO RAIN")
+
+    def get_general_forecast(self):
+        for daily_weather in self.forecast:
+            print(daily_weather.__dict__)
